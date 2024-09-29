1341bff25a7e490b0e48c5edde3b303839883130
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-29 22:31:52 +0600

    validation done

--- js/expense.js
+++ js/expense.js
@@ -1,4 +1,3 @@
-
 // Calculate your
 document.getElementById('calculate').addEventListener('click',function(){
     const income = parseFloat(document.getElementById('income').value);
@@ -9,32 +8,36 @@ document.getElementById('calculate').addEventListener('click',function(){
 
     const expenses = softwer + courses + internet + utility;
     const balance = income - expenses;
-    
-
     const totalExpense = document.getElementById('total-expense')
     const totalBalance = document.getElementById('total-balance')
-    
-    totalBalance.innerText = balance.toFixed(2);
-    totalExpense.innerText = expenses.toFixed(2);
-
     const history = document.getElementById('result-history');
-    history.classList.remove('hidden')
 
+    
     // history functionality
     const historyCard = document.createElement('div')
+    const list =  document.getElementById('history-tab')
     historyCard.innerHTML = `
     <div class = "shadow-lg bg-yellow-100 p-4 my-2 border-l-4 rounded-lg border-green-500">
     <p>Time: ${new Date()}</p>
-    <p class = 'font-semibold text-xl'>Income: $${income}</p> 
-    <p class = 'font-semibold text-xl'>Expense: $${expenses}</p> 
-    <p class = 'font-semibold text-xl'>Income: $${balance}</p>
+    <p class = 'font-semibold text-xl'>Income: $${income.toFixed(2)}</p> 
+    <p class = 'font-semibold text-xl'>Expense: $${expenses.toFixed(2)}</p> 
+    <p class = 'font-semibold text-xl'>Income: $${balance.toFixed(2)}</p>
     </div>
     `
-   const list =  document.getElementById('history-tab')
-   list.insertBefore(historyCard, list.firstChild)
-    
+   
+   if ( (income > expenses) && (softwer >= 0 && courses >=0 && utility >=0 && internet >=0 ) ){
+       totalBalance.innerText = balance.toFixed(2);
+       totalExpense.innerText = expenses.toFixed(2);
+       history.classList.remove('hidden')
+       list.insertBefore(historyCard, list.firstChild)
+    }
+    else {
+        alert('something is worng went, please try again')
+        history.classList.add('hidden')
+    }
 })
 
+// savings functionality
 document.getElementById('saving-btn').addEventListener('click', function(){
     const income = parseFloat(document.getElementById('income').value);
     const softwer = parseFloat(document.getElementById('softwer').value);
@@ -45,10 +48,15 @@ document.getElementById('saving-btn').addEventListener('click', function(){
 
     const expenses = softwer + courses + internet + utility;
     const balance = income - expenses;
-    
     const savingBalance = (balance * savings) / 100;
-    document.getElementById('saving-text').innerText = savingBalance.toFixed(2);
-    document.getElementById('remaining-balance').innerText = (balance - savingBalance).toFixed(2);
+
+    if ( balance >= savingBalance && savingBalance > 0 ){
+        document.getElementById('saving-text').innerText = savingBalance.toFixed(2);
+        document.getElementById('remaining-balance').innerText = (balance - savingBalance).toFixed(2);
+    }else{
+        alert('insufficent balance')
+    }
+    
 })
 
 
